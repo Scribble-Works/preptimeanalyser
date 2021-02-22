111c4e9621acd64e1ebd4cc2f9c49aa6d7607a8d
Auricdhero <datauric@gmail.com>
2021-02-22 22:53:52 +0000

    about page added

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,107 @@
 <template>
-    <div>
-        
+  <div>
+    <div class="row">
+      <div class="col about">
+        <h4>About</h4>
+        <h2>Bringing the benefits of PrepTime Analyser to Evvery Educator</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+          delectus, hic voluptatibus repellat magnam pariatur ad animi tempora
+          voluptatum? Architecto corrupti error esse quod earum numquam ad ullam
+          aspernatur doloribus?
+        </p>
+      </div>
     </div>
+    <div class="row">
+      <h3>Pricing</h3>
+      <div class="col">
+        <h3>Free</h3>
+        <h6>Free</h6>
+        <button class="">Start</button>
+        <hr class="hr-light" />
+        <ul>
+          <li>Feature 1</li>
+          <li>Feature 2</li>
+          <li>Feature 3</li>
+          <li>Feature 4</li>
+        </ul>
+      </div>
+      <div class="col">
+        <h3>Basic</h3>
+        <h6>Gh₵ 10 / month</h6>
+        <button class="">Buy</button>
+        <hr class="hr-light" />
+        <ul>
+          <li>Feature 1</li>
+          <li>Feature 2</li>
+          <li>Feature 3</li>
+          <li>Feature 4</li>
+        </ul>
+      </div>
+      <div class="col">
+        <h3>Standard</h3>
+        <h6>Gh₵ 80 / month</h6>
+        <button class="">Buy</button>
+        <hr class="hr-light" />
+        <ul>
+          <li>Feature 1</li>
+          <li>Feature 2</li>
+          <li>Feature 3</li>
+          <li>Feature 4</li>
+        </ul>
+      </div>
+      <div class="col">
+        <h3>Enterprise</h3>
+        <h6>Gh₵ 250 / month</h6>
+        <button class="">Buy</button>
+        <hr class="hr-light" />
+        <ul>
+          <li>Feature 1</li>
+          <li>Feature 2</li>
+          <li>Feature 3</li>
+          <li>Feature 4</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <img
+          src="~assets/images/accra_col1.jpg"
+          height="235px"
+          width="313px"
+          alt=""
+        />
+      </div>
+      <div class="col">
+        <img src="~assets/images/nap.jpg" height="235px" width="313px" alt="" />
+      </div>
+      <div class="row">
+        <h4>Analyse from Google Forms</h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
+          accusamus sint! Eligendi asperiores delectus, quam dolore veritatis
+          id! Error consequatur officiis laudantium reprehenderit eius ad id
+          dolore possimus deserunt! Ab.
+        </p>
+      </div>
+    </div>
+
+    <div>
+      <h6>PredTime Analyser In Action</h6>
+      <h3>Everything you need to know about PrepTime Analyser</h3>
+      <button>Blog</button>
+    </div>
+    <div class="row">
+      <div class="col">
+        <img src="~assets/images/analser3.png" height="286px" width="381px" alt="">
+      </div>
+      <div class="col">
+        <img src="~assets/images/google-analytics-dashboard-business-analytics-computer-icons-business.png" height="208px" width="208px" alt="">
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+</style>
