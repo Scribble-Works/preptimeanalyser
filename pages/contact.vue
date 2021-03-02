15cb12522ea6698dd20edf7eda76647f6cf414b3
Auricdhero <datauric@gmail.com>
2021-03-02 13:07:33 +0000

    would finish up with the styling

---
+++ pages/contact.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="container">
+      <form action="">
+          <h2>GET IN TOUCH</h2>
+          <div class="row">
+              <div class="col">
+                  <h5>Tell us your name</h5>
+                  <input name="name" type="text" placeholder="Micheal Martey">
+              </div>
+              <div class="col">
+                  <h5>Enter kyour telephone number</h5>
+                  <input name="tel" type="text" placeholder="0244444444">
+              </div>
+          </div>
+          <div class="row">
+              <div class="col">
+                  <h5>Enter yoiur email address</h5>
+                  <input name="email" type="text" placeholder="mikeMartey@gmail.com">
+              </div>
+          </div>
+          <div class="msg">
+              <h5>Your message</h5>
+              <textarea name="message" placeholder="How can we help you?" id="" cols="60" rows="50"></textarea>
+          </div>
+          <button>Send</button>
+      </form>
+    </div>
+  </div>
+</template>
+<style>
+</style>
--- pages/started.vue
+++ pages/started.vue
@@ -1,11 +1,52 @@
 <template>
   <div>
     <div class="container">
-        
+      <div class="img">
+        <img
+          src="~assets/images/privacy.png"
+          height="300px"
+          width="400px"
+          alt=""
+        />
+      </div>
+      <div>
+        <h6>PrepTime Analyser Privacy Policy</h6>
+        <h3>
+          When you use our services, your're trusting us with your information.
+          We understand this is a big responsibility and owrk to protect your
+          information and pit you in control.
+        </h3><br>
+        <p>
+          This Privacy Policy is meant to help you understand what information
+          we collect, why we collect it and how you can update it, manage,
+          export and delete your information.
+        </p>
+      </div>
+    </div>
+    <hr />
+    <div class="container">
+      <div class="img">
+        <img
+          src="~assets/images/sheet_analyze.png"
+          height="211px"
+          width="388px"
+          alt=""
+        />
+      </div><br>
+      <div class="captions">
+        <h4>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae
+          quos cum fugiat perspiciatis ad nesciunt. Atque, veritatis similique
+          explicabo repellendus laboriosam culpa magnam reprehenderit suscipit
+          consequatur nesciunt eum repudiandae! Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Quis obcaecati soluta repudiandae et
+          omnis nostrum dolore nemo? Eum, odio, soluta iste pariatur modi fugiat
+          sapiente dolore est eligendi, quia dignissimos.
+        </h4>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-
 </style>
