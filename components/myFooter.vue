371d4e3f8ed0d99f427bdb6795d70ef78be1fbe1
Auricdhero <datauric@gmail.com>
2021-03-03 01:06:33 +0000

    Fixed footer

--- components/myFooter.vue
+++ components/myFooter.vue
@@ -2,29 +2,33 @@
   <div>
     <footer>
       <div class="container-fluid">
-        <div class="row justify-content-between">
-          <div class="col-6 col-sm-4">
-            <NuxtLink to="/"><img src="~assets/images/PrepTime analyser bw.png" height="70px" width="auto" alt=""></NuxtLink>
-          </div>
+        <div class="img-link">
+          <NuxtLink to="/"
+            ><img
+              src="~assets/images/PrepTime analyser bw.png"
+              height="70px"
+              width="auto"
+              alt=""
+          /></NuxtLink>
         </div>
 
-        <div class="row justify-content-between">
-          <div class="col-md-4">
+        <div class="row justify-content-between footer-docs">
+          <div class="col">
             <NuxtLink class="footer-links" to="">Privacy</NuxtLink>
           </div>
-          <div class="col-md-4">
+          <div class="col">
             <NuxtLink class="footer-links" to="">Terms</NuxtLink>
           </div>
-          <div class="col-md-4">
-            <NuxtLink class="footer-links" to="">About PrepTime Analyser</NuxtLink>
+          <div class="col-sm-6">
+            <NuxtLink class="footer-links" to=""
+              >About PrepTime Analyser</NuxtLink
+            >
           </div>
-           <div class="col-md-4">
+          <div class="col-sm-1">
             <NuxtLink class="footer-links" to="">Feedback</NuxtLink>
           </div>
         </div>
-        <div class="row justify-content-between">
-         
-        </div>
+        <div class="row justify-content-between"></div>
       </div>
     </footer>
   </div>
@@ -33,21 +37,27 @@
 export default {};
 </script>
 <style>
-  footer {
-    position: absolute;
-    width: 2020px;
-    height: 73px;
-    left: 0px;
-    top: 1014px;
-    background-color: rgba(196, 196, 196, 1);
-  }
-  .footer-links {
-    color: black;
-  }
-  /* .row{
+footer {
+  position: absolute;
+  width: 2020px;
+  height: 73px;
+  left: 0px;
+  top: 1014px;
+  background-color: rgba(196, 196, 196, 1);
+}
+.footer-links {
+  color: black;
+}
+/* .row{
     display: inline-block;
   } */
-  .col-md-4{
-    display: inline;
-  }
+.img-link{
+  display: inline;
+}
+
+.footer-docs{
+  position: absolute;
+  left: 290px;
+  top: 19px;
+}
 </style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -2,28 +2,38 @@
   <div>
     <div class="container">
       <form action="">
-          <h2>GET IN TOUCH</h2>
-          <div class="row">
-              <div class="col">
-                  <h5>Tell us your name</h5>
-                  <input name="name" type="text" placeholder="Micheal Martey">
-              </div>
-              <div class="col">
-                  <h5>Enter kyour telephone number</h5>
-                  <input name="tel" type="text" placeholder="0244444444">
-              </div>
+        <h2>GET IN TOUCH</h2>
+        <div class="row">
+          <div class="col">
+            <h5>Tell us your name</h5>
+            <input name="name" type="text" placeholder="Micheal Martey" />
           </div>
-          <div class="row">
-              <div class="col">
-                  <h5>Enter yoiur email address</h5>
-                  <input name="email" type="text" placeholder="mikeMartey@gmail.com">
-              </div>
+          <div class="col">
+            <h5>Enter your telephone number</h5>
+            <input name="tel" type="text" placeholder="0244444444" />
           </div>
-          <div class="msg">
-              <h5>Your message</h5>
-              <textarea name="message" placeholder="How can we help you?" id="" cols="60" rows="50"></textarea>
+        </div>
+        <div class="row">
+          <div class="col">
+            <h5>Enter yoiur email address</h5>
+            <input
+              name="email"
+              type="text"
+              placeholder="mikeMartey@gmail.com"
+            />
           </div>
-          <button>Send</button>
+        </div>
+        <div class="msg">
+          <h5>Your message</h5>
+          <textarea
+            name="message"
+            placeholder="How can we help you?"
+            id=""
+            cols="60"
+            rows="20"
+          ></textarea>
+        </div>
+        <button>Send</button>
       </form>
     </div>
   </div>
