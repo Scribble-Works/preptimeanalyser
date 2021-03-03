aedc28588f88da9acf235ac8bbbc0510fc32223d
Auricdhero <datauric@gmail.com>
2021-03-03 03:24:11 +0000

    would con't with the styling later

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col about">
         <h4>About</h4>
-        <h2>Bringing the benefits of PrepTime Analyser to Evvery Educator</h2>
+        <h2>Bringing the benefits of PrepTime Analyser to Every Educator</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
           delectus, hic voluptatibus repellat magnam pariatur ad animi tempora
@@ -12,7 +12,7 @@
         </p>
       </div>
     </div>
-    <div class="row">
+    <div class="row price">
       <h3>Pricing</h3>
       <div class="col">
         <h3>Free</h3>
@@ -64,7 +64,7 @@
       </div>
     </div>
 
-    <div class="row">
+    <div class="row studs">
       <div class="col">
         <img
           src="~assets/images/accra_col1.jpg"
@@ -87,7 +87,7 @@
       </div>
     </div>
 
-    <div>
+    <div class="evrtng">
       <h6>PrepTime Analyser In Action</h6>
       <h3>Everything you need to know about PrepTime Analyser</h3>
       <button>Blog</button>
@@ -114,4 +114,78 @@
 </template>
 
 <style>
+.about {
+  position: absolute;
+  width: 310px;
+  height: 180px;
+  left: 268px;
+  top: 81px;
+
+  background: #fff9f9;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+
+.about h4 {
+  position: absolute;
+  top: 18px;
+  width: 68px;
+  height: 22px;
+  font-size: 15px;
+  font-family: Roboto;
+  font-weight: normal;
+  line-height: 18px;
+  display: flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.65);
+}
+
+.about h2 {
+  position: absolute;
+  left: 10px;
+  top: 47px;
+  width: 262px;
+  font-family: Squada One;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 19px;
+  color: #000000;
+}
+
+.about p{
+  position: absolute;
+  width: 282px;
+  height: 76px;
+  left: 10px;
+  top: 85px;
+  font-family: Tuffy;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 13px;
+  line-height: 19px;
+
+  color: rgba(0, 0, 0, 0.76);
+}
+
+.price{
+  position: absolute;
+  width: 100%;
+  height: 263px;
+  top: 274px;
+  background: #3E4F5C;
+}
+
+.studs{
+  position: absolute;
+  top: 536px;
+}
+
+.evrtng{
+  position: absolute;
+  top: 752px;
+}
+.evrtng img{
+  position: absolute;
+  top: 812px;
+}
 </style>
