222bb46aca0e39bfea24e6e5eafffa902e8d44f5
Auricdhero <datauric@gmail.com>
2021-03-11 01:58:53 +0000

    doing the finish up and mobile view quite complex

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="about">
     <div class="container-about">
       <h5>About</h5>
       <h3>Bringing the benefits of Preptime Analyser to every educator</h3>
@@ -11,9 +11,9 @@
       </p>
     </div>
 
-    <div class="row pricing">
+    <div class="row justify-content-between pricing">
       <h3>Pricing</h3>
-      <div class="col">
+      <div class="container col-2">
         <h3>Free</h3>
         <h5>Free</h5>
         <div class="btn">
@@ -27,7 +27,7 @@
           <li>Feature 4</li>
         </ul>
       </div>
-      <div class="col">
+      <div class="container col-2">
         <h3>Basic</h3>
         <h5>GH₵10 / month</h5>
         <div class="btn">
@@ -41,8 +41,8 @@
           <li>Feature 4</li>
         </ul>
       </div>
-      <div class="col">
-        <h3>Basic</h3>
+      <div class="container col-2">
+        <h3>Standard</h3>
         <h5>GH₵80 / month</h5>
         <div class="btn">
           <a class="btn btn-secondary btn-sm" href="#" role="button">Buy</a>
@@ -55,8 +55,8 @@
           <li>Feature 4</li>
         </ul>
       </div>
-      <div class="col">
-        <h3>Basic</h3>
+      <div class="container col-2">
+        <h3>Enterprice</h3>
         <h5>GH₵250 / month</h5>
         <div class="btn">
           <a class="btn btn-secondary btn-sm" href="#" role="button">Buy</a>
@@ -70,40 +70,19 @@
         </ul>
       </div>
     </div>
-    <div class="row img justify-content-center">
-      <div class="col">
-        <img
-          src="~assets/images/accra_col1.jpg"
-          height="235px"
-          width="313px"
-          alt=""
-        />
-      </div>
-      <div class="col">
-        <h3>Analyse from Google Forms</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
-          ab possimus obcaecati alias dicta libero omnis laboriosam, quia eaque
-          porro delectus voluptatum quaerat nulla, voluptatem minus dolorem
-          consequuntur aspernatur nesciunt.
-        </p>
-      </div>
-      <div class="col">
-        <img src="~assets/images/nap.jpg" height="235px" width="313px" alt="" />
-      </div>
-    </div>
+    
     <div class="row blog">
-      <div class="col">
+      <div class="col-2">
         <h6>PrepTime Analyser in Action</h6>
         <h3>Everything you need to know about PrepTime Analyser</h3>
         <div class="btn">
           <a class="btn btn-secondary btn-blog" href="#" role="button">Blog</a>
         </div>
       </div>
-      <div class="col">
+      <div class="col-auto">
         <img src="~assets/images/analser2.png" height="206px" width="281px" alt="" />
       </div>
-      <div class="col">
+      <div class="col-auto">
         <img
           src="~assets/images/google-analytics-dashboard-business-analytics-computer-icons-business.png"
           height="216px"
@@ -116,6 +95,40 @@
 </template>
 
 <style>
+.about{
+  position: relative;
+}
+.about:before{
+  content: '';
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.4;
+  background: #EFEEEE url(~assets/images/preptime_bg.png) repeat-x;
+  background-size: cover;
+}
+
+.blog{
+  position: relative;
+  height: 300px;
+  top: 350px;
+  left: 0px;
+}
+.blog:before{
+  content: '';
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.4;
+  background: #EFEEEE url(~assets/images/preptime_bg.png) no-repeat;
+  background-size: cover;
+}
 .container-about {
   background: #fff9f9;
   position: absolute;
@@ -129,16 +142,28 @@
 .pricing {
   background: #3e4f5c;
   position: relative;
-  width: auto;
+  width: 100%;
   height: 340px;
   top: 300px;
 }
-.img {
+.pricing h3{
   position: relative;
-  top: 310px;
+  left: 80px;
 }
-.blog{
+.pricing .col-2{
   position: relative;
-  top: 300px;
+  background: #FFFFFF; 
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  top: -15px;
+}
+.blog .col-2 h3{
+  width: 250px;
+}
+.blog .col-2{
+  position: relative;
+  left: 250px;
+  height: 204px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background: #FFFFFF;
 }
 </style>
