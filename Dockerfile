dc8e8cd56a017d884c7ba4e1af8a0fb6e783721f
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2021-03-17 20:30:16 +0000

    basic Docker setup

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ WORKDIR /usr/src/app
 
 COPY . ./
 
+ENV NUXT_HOST=0.0.0.0
+ENV NUXT_PORT=8080
+
 RUN yarn
 
 RUN yarn build
--- nuxt.config.js
+++ nuxt.config.js
@@ -66,10 +66,5 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
 
-  },
-  server: {
-    port: process.env.PORT || 3000, // default: 3000
-    host: 'localhost', // default: localhost,
-    timing: false
   }
 }
