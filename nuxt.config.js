5790ed76e1eb99ad1184a3979c24d64f000826ca
Auricdhero <datauric@gmail.com>
2021-03-25 01:55:18 +0000

    fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~layouts/style.css'
+    '~layouts/global.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
